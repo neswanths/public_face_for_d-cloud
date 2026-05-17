612484bf4b3a6f091968f4f253b5fc02210aab95
Neswanth Pasupuleti <neswanths@gmail.com>
2026-05-18 00:08:47 +0530

    Change base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/public_face_for_d-cloud/',
+  base: '/',  // ← change this from '/public_face_for_d-cloud/' to '/'
 })
